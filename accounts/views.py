ba4274dbd882b6e800c51e0e0c0bf4b9d0fc029a
WayneMusungu <waynemuyera17@gmail.com>
2022-11-03 11:05:49 +0300

    Django messages implementation

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .forms import UserForm
 from .models import User
+from django.contrib import messages
 
 # Create your views here.
 
@@ -31,6 +32,7 @@ def registerUser(request):
             user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
             user.role = User.CUSTOMER
             user.save()
+            messages.success(request, "Your account has been registered successfully!!")
             print('The user has been created successfuly')            
             return redirect('registerUser')
         else:
