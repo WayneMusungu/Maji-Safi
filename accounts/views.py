3b5b5a1dd7fb7e2bee32662de69342a65867410f
WayneMusungu <waynemuyera17@gmail.com>
2022-11-04 10:05:41 +0300

    Create Vendor registration form

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from vendor.forms import VendorForm
 from .forms import UserForm
-from .models import User
+from .models import User, UserProfile
 from django.contrib import messages
 
 # Create your views here.
@@ -47,4 +48,40 @@ def registerUser(request):
 
 
 def registerVendor(request):
-    return render(request, 'accounts/registerVendor.html')
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        vendor_form = VendorForm(request.POST, request.FILES)
+        if form.is_valid() and vendor_form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            
+            user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
+            user.role = User.WATER_SUPPLIER
+            user.save()
+            vendor = vendor_form.save(commit = False)
+            vendor.user = user
+            """
+            Get the user profile fom the UserProfile Model.
+            When the user.save is trigerred, 
+            Signals will create the user profile of the user
+            """
+            user_profile = UserProfile.objects.get(user=user)
+            vendor.user_profile = user_profile
+            vendor.save()
+            messages.success(request, "Your account has been registered successfully!! Kindly wait for approval from the admin")
+            return redirect('registerVendor')
+
+        else:
+            print('invalid form')
+            print(form.errors)
+    else:
+        form = UserForm()
+        vendor_form = VendorForm()
+    context ={
+        'form':form,
+        'vendor_form': vendor_form,
+    }
+    return render(request, 'accounts/registerVendor.html', context)
--- vendor/forms.py
+++ vendor/forms.py
@@ -1,23 +1,7 @@
 from django import forms
+from .models import Vendor
 
-
-class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
-    confirm_password = forms.CharField(widget=forms.PasswordInput())
+class VendorForm(forms.ModelForm):
     class Meta:
-        model = User
-        fields = ['first_name', 'last_name', 'username', 'email', 'password']
-        
-    def clean(self):
-        """
-        The super function give us the ability to overide
-        the clean method which is an inbuilt function
-        """
-        cleaned_data = super(UserForm, self).clean()
-        password = cleaned_data.get('password')
-        confirm_password = cleaned_data.get('confirm_password')
-        
-        if password != confirm_password:
-            raise forms.ValidationError(
-                'Password do not match!!'
-            )
+        model = Vendor
+        fields = ['vendor_name', 'vendor_license']
