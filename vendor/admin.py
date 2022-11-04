cf2b4d6a9c94250bcdd7e26ea6c200002c1613e9
WayneMusungu <waynemuyera17@gmail.com>
2022-11-04 10:58:09 +0300

    Create Vendor Admin Configuration

--- vendor/admin.py
+++ vendor/admin.py
@@ -4,4 +4,9 @@ from vendor.models import Vendor
 
 # Register your models here.
 
-admin.site.register(Vendor)
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ('user', 'vendor_name', 'is_approved', 'created_at')
+    list_display_links = ('user', 'vendor_name')
+    
+
+admin.site.register(Vendor, VendorAdmin)
