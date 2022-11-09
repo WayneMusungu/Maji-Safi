bde0c52906838d6c1899ac8fed5ad3f454ef6b9a
WayneMusungu <waynemuyera17@gmail.com>
2022-11-09 09:17:15 +0300

    Reset password feature

--- accounts/views.py
+++ accounts/views.py
@@ -48,9 +48,11 @@ def registerUser(request):
             """
             Create a helper function to send the verification email to the registered User
             """
-            send_email_verification(request, user)
+            subject = 'Account Activation'
+            email_template = 'accounts/emails/account_email_verification.html'
+            send_email_verification(request, user, subject, email_template)
             
-            messages.success(request, "Your account has been registered successfully!!")
+            messages.success(request, "Your account has been created, an activation link has been sent to your email")
             print('The user has been created successfuly')            
             return redirect('registerUser')
         else:
@@ -96,9 +98,12 @@ def registerSupplier(request):
             """
             Create a helper function to send the verification email to the registered Supplier
             """
-            send_email_verification(request, user)
+            subject = 'Account Activation'
+            email_template = 'accounts/emails/account_email_verification.html'
+
+            send_email_verification(request, user, subject, email_template)
             
-            messages.success(request, "Your account has been registered successfully!! Kindly wait for approval from the admin")
+            messages.success(request, "Your account has been created, an activation link has been sent to your email. Kindly wait for approval from the admin")
             return redirect('registerSupplier')
 
         else:
@@ -200,3 +205,71 @@ def check_role_supplier(user):
 def supplierDashboard(request):
     return render(request, 'accounts/supplierDashboard.html')
 
+
+def forgot_password(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        
+        if User.objects.filter(email=email).exists():
+            user = User.objects.get(email__exact=email)
+            
+            """
+            Create a helper function for password reset email
+            """
+            
+            # send_password_reset_email(request, user)
+            subject = 'Password Reset'
+            email_template = 'accounts/emails/reset_password_email.html'
+            send_email_verification(request, user, subject, email_template)
+            
+            messages.success(request, 'Password reset link has been sent to your email address')
+            return redirect('login')
+            
+        else:
+            messages.error(request, 'An account with that email does not exist')
+            return redirect('forgot_password')
+            
+    return render(request, 'accounts/forgot_password.html')
+
+
+def reset_password_validate(request, uidb64, token):
+    """
+    Validate the user by decoding the token and the user pk
+    """
+    try:
+        """Get the encoded uid and decode it."""
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = User._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+        
+    if user is not None and default_token_generator.check_token(user, token):
+        """
+        Store the uid inside the session because we need the pk to reset the password
+        """
+        request.session['uid'] = uid
+        messages.info(request, 'A password reset link has been sent to your email')
+        return redirect('reset_password')
+        
+    else:
+        messages.error(request, 'This link has expired')
+        return redirect(myAccount)
+
+
+def reset_password(request):
+    if request.method == 'POST':
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password == confirm_password:
+            pk = request.session.get('uid')
+            user = User.objects.get(pk=pk)
+            user.set_password(password)
+            user.is_active = True
+            user.save()
+            messages.success(request, 'Password reset successful')
+            return redirect('login')
+        else:
+            messages.error(request, 'Password do not match!')
+            return redirect('reset_password')
+    return render(request, 'accounts/reset_password.html')
