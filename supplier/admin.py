f5bf7ea07e467ea4c06dc3088e51c5e3b065b470
WayneMusungu <waynemuyera17@gmail.com>
2022-11-09 11:18:43 +0300

    Create a list_editable property in SupplierAdmin

--- supplier/admin.py
+++ supplier/admin.py
@@ -7,6 +7,7 @@ from supplier.models import Supplier
 class SupplierAdmin(admin.ModelAdmin):
     list_display = ('user', 'supplier_name', 'is_approved', 'created_at')
     list_display_links = ('user', 'supplier_name')
+    list_editable = ('is_approved',)
     
 
 admin.site.register(Supplier, SupplierAdmin)
