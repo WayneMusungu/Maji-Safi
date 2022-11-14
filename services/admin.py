4d53094f9d34944978a7eabf4dea90be42261976
WayneMusungu <waynemuyera17@gmail.com>
2022-11-14 13:01:46 +0300

    Customize the service admin panel

--- services/admin.py
+++ services/admin.py
@@ -3,7 +3,20 @@ from .models import Category, Product
 
 # Register your models here.
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('supplier', 'category_name', 'updated_at')
+    prepopulated_fields = {'slug': ('category_name',)}
+    search_fields = ('category_name', 'supplier__supplier_name')
+    
+    
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('water_brand_title', 'category', 'supplier', 'price', 'is_available', 'updated_at')
+    prepopulated_fields = {'slug': ('water_brand_title',)}
+    search_fields = ('water_brand_title', 'category__category_name', 'supplier__supplier_name', 'price')
+    list_filter = ('is_available',)
 
-admin.site.register(Category)
-admin.site.register(Product)
+    
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
 
