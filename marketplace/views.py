baedbaf2c56d2295fa2f649cda0e48a1f1f95411
WayneMusungu <waynemuyera17@gmail.com>
2022-11-21 12:56:41 +0300

    List all water shops on water market place

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from supplier.models import Supplier
 
 # Create your views here.
 
 def marketplace(request):
-    return render(request, 'marketplace/listings.html')
+    suppliers = Supplier.objects.filter(is_approved=True, user__is_active=True)
+    supplier_count = suppliers.count()
+    context = {
+        "suppliers":suppliers,
+        "supplier_count":supplier_count,
+    }
+    return render(request, 'marketplace/listings.html', context)
