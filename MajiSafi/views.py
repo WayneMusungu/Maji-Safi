6ea130a09620933414f5455c13ddde5ae83af871
WayneMusungu <waynemuyera17@gmail.com>
2022-11-21 10:29:22 +0300

    Show suppliers on homepage

--- MajiSafi/views.py
+++ MajiSafi/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from supplier.models import Supplier
+
 
 def home(request):
-    return render(request, "home.html")
+    """
+    Show approved and active Suppliers on the homepage
+    """
+    suppliers = Supplier.objects.filter(is_approved=True, user__is_active=True)[:10]
+    print(suppliers)
+    context = {
+        "suppliers":suppliers,
+    }
+    return render(request, "home.html", context)
