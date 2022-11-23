4587b8d86c89ae06a8dcf84c978a82da0c050af2
WayneMusungu <waynemuyera17@gmail.com>
2022-11-23 07:30:21 +0300

    Create Model Cart

--- marketplace/admin.py
+++ marketplace/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Cart
 
 # Register your models here.
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('user', 'product', 'quantity', 'updated_at')
+
+admin.site.register(Cart, CartAdmin)
--- marketplace/models.py
+++ marketplace/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from accounts.models import User
+from services.models import Product
 
 # Create your models here.
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __unicode__(self):
+        return self.user
