05aa5a49bd8ab9e847b2f97baccf9833b9d9e1fe
WayneMusungu <waynemuyera17@gmail.com>
2022-11-30 09:59:05 +0300

    Get Cart Amounts Function to Get Subtotal and Grand Total of Cart Products

--- MajiSafi/settings.py
+++ MajiSafi/settings.py
@@ -75,6 +75,7 @@ TEMPLATES = [
                 'accounts.context_processors.get_supplier',
                 'accounts.context_processors.get_google_api',
                 'marketplace.context_processors.get_cart_counter',
+                'marketplace.context_processors.get_cart_amounts',
             ],
         },
     },
--- marketplace/context_processors.py
+++ marketplace/context_processors.py
@@ -20,3 +20,19 @@ def get_cart_counter(request):
         except:
             cart_count = 0
     return dict(cart_count=cart_count)
+
+
+def get_cart_amounts(request):
+    subtotal = 0
+    tax = 0
+    grand_total = 0
+    if request.user.is_authenticated:
+        cart_items = Cart.objects.filter(user=request.user)
+        for item in cart_items:
+            product = Product.objects.get(pk=item.product.id)
+            subtotal += (product.price * item.quantity)
+            
+        grand_total = subtotal + tax
+        print(subtotal)
+        print(grand_total)
+        return dict(subtotal=subtotal, tax=tax, grand_total=grand_total)
