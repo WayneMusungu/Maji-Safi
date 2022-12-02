87c2e47f94f05d69bf5cc163a9be8ffaa6f363a5
WayneMusungu <waynemuyera17@gmail.com>
2022-12-02 09:20:58 +0300

    Create Supplier Admin Configuration

--- supplier/admin.py
+++ supplier/admin.py
@@ -4,4 +4,10 @@ from supplier.models import Supplier
 
 # Register your models here.
 
-admin.site.register(Supplier)
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = ('user', 'supplier_name', 'is_approved', 'created_at')
+    list_display_links = ('user', 'supplier_name')
+    list_editable = ('is_approved',)
+    
+
+admin.site.register(Supplier, SupplierAdmin)
