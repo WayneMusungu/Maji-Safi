3d26152800e219bd396f0c103cfeec166979cb49
WayneMusungu <waynemuyera17@gmail.com>
2022-12-05 09:39:29 +0300

    User account activation

--- accounts/urls.py
+++ accounts/urls.py
@@ -13,9 +13,7 @@ urlpatterns = [
    
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
     
-    path('forgot_password/', views.forgot_password, name='forgot_password'),
-    path('reset_password_validate/<uidb64>/<token>/', views.reset_password_validate, name='reset_password_validate'),
-    path('reset_password/', views.reset_password, name='reset_password'),
+    
     
 
 ]
