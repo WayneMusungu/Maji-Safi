8e64e30bd1e34844d1834f657d173207f4385a11
WayneMusungu <waynemuyera17@gmail.com>
2022-12-07 11:18:44 +0300

    Create service app and its models

--- MajiSafi/settings.py
+++ MajiSafi/settings.py
@@ -35,7 +35,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
     'supplier.apps.SupplierConfig',
-    'menu.apps.MenuConfig',
+    'services.apps.ServicesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -84,11 +84,23 @@ WSGI_APPLICATION = 'MajiSafi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+'default': {
+'ENGINE': 'django.db.backends.postgresql',
+'NAME': config('DB_NAME'),
+'USER': config('DB_USER'),
+'PASSWORD': config('DB_PASSWORD'),
+'HOST': config('DB_HOST'),
+'PORT': '5432',
+}
+
 }
 
 
---
+++ services/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from supplier.models import Supplier
+
+# Create your models here.
+
+class Category(models.Model):
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+    category_name = models.CharField(max_length=50, unique=True)
+    """
+    A slug field in Django is used to store and generate valid URLs for your dynamically created web pages.
+    """
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField(max_length=250, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = "categories"
+    
+    def __str__(self):
+        return self.category_name
+    
+class Product(models.Model):
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    water_brand_title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField(max_length=250, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='waterimages')
+    is_available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.water_brand_title
