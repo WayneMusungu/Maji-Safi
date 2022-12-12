cfca0f3a9673d63bf2a8561188ccc20f6d15ecf2
WayneMusungu <waynemuyera17@gmail.com>
2022-12-12 09:05:10 +0300

    Supplier Detail Page Setup

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.marketplace, name='marketplace')
+    path('', views.marketplace, name='marketplace'),
+    path('<slug:supplier_slug>/', views.supplier_detail, name='supplier_detail'),
 ]
