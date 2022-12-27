71db0ac74a16210bb64ec4915c762c5f54d5074e
WayneMusungu <waynemuyera17@gmail.com>
2022-12-27 13:24:31 +0300

    Create Tax Model

--- marketplace/models.py
+++ marketplace/models.py
@@ -13,3 +13,16 @@ class Cart(models.Model):
     
     def __unicode__(self):
         return self.user
+
+
+class Tax(models.Model):
+    tax_type = models.CharField(max_length=20, unique=True)
+    tax_percentage = models.DecimalField(decimal_places=2, max_digits=4, verbose_name='Tax Percentage (%)')
+    is_active = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.tax_type
+    
+    class Meta:
+        verbose_name_plural = "taxes"
+  
