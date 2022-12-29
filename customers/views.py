c9cf45a4a8b473341b0b173a62a102bfb4538068
WayneMusungu <waynemuyera17@gmail.com>
2022-12-29 14:40:04 +0300

    Update Customer Profile Function

--- customers/views.py
+++ customers/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.decorators import login_required
 from accounts.forms import UserProfileForm, UserInfoForm
 from accounts.models import UserProfile
+from django.contrib import messages
 # Create your views here.
 
 @login_required(login_url='login')
@@ -9,12 +10,26 @@ def customerProfile(request):
     # Show the existing values inside the form field of a logged in customer
     profile = get_object_or_404(UserProfile, user=request.user)
     
+    if request.method == 'POST':
+      profile_form = UserProfileForm(request.POST, request.FILES, instance=profile)
+      user_form = UserInfoForm(request.POST, instance=request.user)
+      
+      if profile_form.is_valid() and user_form.is_valid():
+         profile_form.save()
+         user_form.save()
+         messages.success(request, 'Profile updated')
+         return redirect('customerProfile')
+      else:
+         print(profile_form.errors)
+         print(user_form.errors)
     
-    profile_form = UserProfileForm(instance=profile)
-    user_form = UserInfoForm(instance=request.user)
+    else:
+      profile_form = UserProfileForm(instance=profile)
+      user_form = UserInfoForm(instance=request.user)
     
     context = {
        'profile_form': profile_form,
-       'user_form': user_form 
+       'user_form': user_form ,
+       'profile': profile,
     }
     return render(request, 'customers/customerProfile.html', context)
