7ba78538d0a02594c7112b7c7c9e13e4b874a95f
WayneMusungu <waynemuyera17@gmail.com>
2022-12-29 16:11:31 +0300

    Create Ordered Product Model

--- orders/admin.py
+++ orders/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Payment, Order
+from .models import Payment, Order, OrderedProduct
 
 # Register your models here.
 
 admin.site.register(Payment)
 admin.site.register(Order)
+admin.site.register(OrderedProduct)
--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from accounts.models import User
+from services.models import Product
 
 # Create your models here.
 
@@ -55,6 +56,22 @@ class Order(models.Model):
 
     def __str__(self):
         return self.order_number
+    
+    
+class OrderedProduct(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    payment = models.ForeignKey(Payment, on_delete=models.SET_NULL, blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    productitem = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    price = models.FloatField()
+    amount = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.productitem.bottle_size
+
     
     
 
