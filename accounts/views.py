57f31a70189bb6b367f346ed845ffbdce8bfb01b
WayneMusungu <waynemuyera17@gmail.com>
2023-01-06 13:54:23 +0300

    Custom Dashboard Recent Orders

--- accounts/views.py
+++ accounts/views.py
@@ -11,6 +11,7 @@ from django.utils.http import urlsafe_base64_decode
 from django.contrib.auth.tokens import default_token_generator
 from supplier.models import Supplier
 from django.template.defaultfilters import slugify
+from orders.models import Order
 
 
 
@@ -192,7 +193,12 @@ def check_role_customer(user):
 @login_required(login_url='login')
 @user_passes_test(check_role_customer)
 def customerDashboard(request):
-    return render(request, 'accounts/customerDashboard.html')
+    orders = Order.objects.filter(user=request.user, is_ordered=True)[:6]  # Show only six recent orders
+    context = {
+        'orders': orders,
+        'orders_count': orders.count()  # count the number of orders made by the customer
+    }
+    return render(request, 'accounts/customerDashboard.html', context)
 
 
 """
