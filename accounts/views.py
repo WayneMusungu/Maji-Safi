bb21e0aa93dee397129170c2425f77a3f55402d6
WayneMusungu <waynemuyera17@gmail.com>
2023-01-09 10:51:47 +0300

    Total Orders Supplier Dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -215,11 +215,15 @@ def check_role_supplier(user):
 @login_required(login_url='login')
 @user_passes_test(check_role_supplier)
 def supplierDashboard(request):
+    supplier = Supplier.objects.get(user=request.user)
+    orders = Order.objects.filter(suppliers__in=[supplier.id], is_ordered=True).order_by('-created_at')
+    # print(orders)
     # supplier = Supplier.objects.get(user=request.user)
-    # context = {
-    #     'supplier': supplier,
-    # }
-    return render(request, 'accounts/supplierDashboard.html')
+    context = {
+        'orders': orders,
+        'orders_count': orders.count()
+    }
+    return render(request, 'accounts/supplierDashboard.html', context)
 
 
 def forgot_password(request):
