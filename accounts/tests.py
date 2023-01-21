780c77c854bf21b66c7ffad079a33a7bd6becad6
WayneMusungu <waynemuyera17@gmail.com>
2023-01-21 18:48:15 +0300

    Create User Test

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTest(TestCase):
+    def test_create_user_with_email_successful(self):
+        """Test creating a user with an email is successful"""
+        email = 'test@example.com'
+        password = 'testpass321'
+        username = 'test1'
+        first_name = 'Kerei'
+        last_name = 'Musungu'
+        user = get_user_model().objects.create_user(
+            email=email,
+            username = username,
+            first_name = first_name,
+            last_name = last_name,
+            password=password
+        )
+        self.assertEqual(user.email, email)
+        self.assertEqual(user.username, username)
+        self.assertEqual(user.first_name, first_name)
+        self.assertEqual(user.last_name, last_name)
+        self.assertTrue(user.check_password(password))
