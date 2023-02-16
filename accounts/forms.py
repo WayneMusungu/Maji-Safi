7aef25ade57c45b7ab832724d2814cc825c9ae68
WayneMusungu <waynemuyera17@gmail.com>
2023-02-16 12:03:57 +0300

    Implement Strong Password Auth

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,15 +3,23 @@ from .models import User, UserProfile
 from .validators import allow_only_images_valdators
 from phonenumber_field.formfields import PhoneNumberField
 from phonenumber_field.widgets import PhoneNumberPrefixWidget
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+import re
+
+def validate_special_character(value):
+    pattern = r'[\W_]+'
+    if not re.search(pattern, value):
+        raise ValidationError('Password must contain at least one special character eg."~!@#$%^&*"')
 
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
+    password = forms.CharField(widget=forms.PasswordInput(), validators=[validate_password, validate_special_character])
     confirm_password = forms.CharField(widget=forms.PasswordInput())
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'password']
-        
+
     def clean(self):
         """
         The super function give us the ability to overide
@@ -20,12 +28,12 @@ class UserForm(forms.ModelForm):
         cleaned_data = super(UserForm, self).clean()
         password = cleaned_data.get('password')
         confirm_password = cleaned_data.get('confirm_password')
-        
+
         if password != confirm_password:
             raise forms.ValidationError(
-                'Password do not match!!'
+                'Passwords do not match!!'
             )
-            
+
 class UserProfileForm(forms.ModelForm):
     address = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'start typing .....', 'required':'required'}))
     profile_picture = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_valdators])
@@ -34,17 +42,14 @@ class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ['profile_picture', 'cover_photo', 'country', 'county', 'town', 'address', 'pin_code']
-        
-        
-class UserInfoForm(forms.ModelForm):  
+
+
+class UserInfoForm(forms.ModelForm):
     phone_number = PhoneNumberField(
         widget = PhoneNumberPrefixWidget(initial='KE')
     )
-    
+
     class Meta:
-       
+
         model = User
         fields = ['first_name', 'last_name', 'phone_number']
-        
-        
-        
