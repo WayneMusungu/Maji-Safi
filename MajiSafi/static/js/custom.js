e8d9660a5a1a76089fe1955c0e49f7b344d01f80
WayneMusungu <waynemuyera17@gmail.com>
2023-10-02 11:15:04 +0300

    Comment out on autocomplete logic in custom.js

--- MajiSafi/static/js/custom.js
+++ MajiSafi/static/js/custom.js
@@ -1,30 +1,30 @@
-let autocomplete;
+// let autocomplete;
 
-function initAutoComplete(){
-autocomplete = new google.maps.places.Autocomplete(
-    document.getElementById('id_address'),
-    {
-        types: ['geocode', 'establishment'],
-        //Country code IN KENYA
-        componentRestrictions: {'country': ['ke']},
-    })
-// function to specify what should happen when the prediction is clicked
-autocomplete.addListener('place_changed', onPlaceChanged);
-}
+// function initAutoComplete(){
+// autocomplete = new google.maps.places.Autocomplete(
+//     document.getElementById('id_address'),
+//     {
+//         types: ['geocode', 'establishment'],
+//         //Country code IN KENYA
+//         componentRestrictions: {'country': ['in']},
+//     })
+// // function to specify what should happen when the prediction is clicked
+// autocomplete.addListener('place_changed', onPlaceChanged);
+// }
 
-function onPlaceChanged (){
-    var place = autocomplete.getPlace();
+// function onPlaceChanged (){
+//     var place = autocomplete.getPlace();
 
-    // User did not select the prediction. Reset the input field or alert()
-    if (!place.geometry){
-        document.getElementById('id_address').placeholder = "Start typing...";
-    }
-    else{
-        console.log('place name=>', place.name)
-    }
+//     // User did not select the prediction. Reset the input field or alert()
+//     if (!place.geometry){
+//         document.getElementById('id_address').placeholder = "Start typing...";
+//     }
+//     else{
+//         console.log('place name=>', place.name)
+//     }
 
-        // get the address components and assign them to the fields
-}
+//         // get the address components and assign them to the fields
+// }
 
 $(document).ready(function(){
     // Add  to Cart
