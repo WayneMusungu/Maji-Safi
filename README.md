3d9ad843db6cb97faff5f5b9284723bd04533348
wayne <waynemuyera17@gmail.com>
2024-09-10 12:26:01 +0300

    Dockerize appliaction

--- README.md
+++ README.md
@@ -17,23 +17,24 @@ cd Maji-Safi
 
 Create your `.env` file and pass in the env variables like in the sample below, Check `.env-sample` file:
 ```bash
-SECRET_KEY=yoursecretkeyhere
+SECRET_KEY = env('SECRET_KEY', default="secret_key")
 DEBUG=True # Set to False when deploying to production
 
 #Email Configuration
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = env('EMAIL_HOST')
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL')
-
-# Add Google API
-GOOGLE_API_KEY=config('GOOGLE_API_KEY')
+EMAIL_BACKEND=your_default_email_backend
+EMAIL_HOST=your_default_email_host
+EMAIL_PORT=587)  # Example default port
+EMAIL_USE_TLS=True
+EMAIL_HOST_USER=your_default_email_user
+EMAIL_HOST_PASSWORD=your_default_email_password
+DEFAULT_FROM_EMAIL=your_default_from_email
 
 # Paypal Configuration
-PAYPAL_CLIENT_ID= config('PAYPAL_CLIENT_ID')
+PAYPAL_CLIENT_ID=your_pay_pal_client_id
+
+# Google Configuration
+GOOGLE_API_KEY=your_default_google_api_key
+
 
 ```
 
@@ -66,25 +67,15 @@ docker --version
 ### Building a Docker image for our application 
 A Docker image is a read-only template that describes how to create a Docker container. To build an optimized docker image of our app with one command, run the command below from the root folder where `Dockerfile` is located.
 ```bash
-docker-compose up -d --build
+docker compose build
+docker compose up -d
 ```
 
-### New Database and SuperUser
-Apply migrations to the application by running the command
-```bash
-docker-compose exec web python manage.py migrate
-```
 
-and create superuser using the command below
+### SuperUser Creation
+Create superuser using the command below
 ```bash
-docker-compose exec web python manage.py createsuperuser
-```
-
-### Running tests
-To run tests, use the following command:
-
-```bash
-docker-compose exec web python manage.py test
+docker exec -it my-app-container python manage.py createsuperuser
 ```
 
 ### View Application
---
+++ static/js/custom.js
@@ -0,0 +1,305 @@
+// let autocomplete;
+
+// function initAutoComplete(){
+// autocomplete = new google.maps.places.Autocomplete(
+//     document.getElementById('id_address'),
+//     {
+//         types: ['geocode', 'establishment'],
+//         //Country code IN KENYA
+//         componentRestrictions: {'country': ['in']},
+//     })
+// // function to specify what should happen when the prediction is clicked
+// autocomplete.addListener('place_changed', onPlaceChanged);
+// }
+
+// function onPlaceChanged (){
+//     var place = autocomplete.getPlace();
+
+//     // User did not select the prediction. Reset the input field or alert()
+//     if (!place.geometry){
+//         document.getElementById('id_address').placeholder = "Start typing...";
+//     }
+//     else{
+//         console.log('place name=>', place.name)
+//     }
+
+//         // get the address components and assign them to the fields
+// }
+
+$(document).ready(function(){
+    // Add  to Cart
+    $('.add_to_cart').on('click', function(e){
+        e.preventDefault();
+        
+        product_id = $(this).attr('data-id');
+        url = $(this).attr('data-url');
+
+
+        // No need to pass the data dictionary because by default the url will contain the product_id
+
+        // data = {
+        //     product_id: product_id,
+        // }
+        
+
+        //  Send the product_id to add to cart view using the Ajax Request
+        // alert(product_id)
+        $.ajax({
+            type: 'GET',
+            url: url,
+            // data: data,
+            success: function(response){
+                // alert(response)
+                console.log(response)
+                if(response.status == 'login_required'){
+                    // console.log(response)
+
+                    // Implement SweetAlert Function
+                    swal(response.message, '', 'info').then(function(){
+                        window.location = '/login';
+                    })
+                }
+                if(response.status == 'Failed'){
+                    swal(response.message, '', 'error')
+                }else{
+                     // console.log(response.cart_counter['cart_count'])
+                $('#cart_counter').html(response.cart_counter['cart_count']);
+                $('#qty-'+product_id).html(response.qty);
+
+
+                // subtotal, tax and the grand_total
+                applyCartAmounts(
+                    response.cart_amount['subtotal'],
+                    response.cart_amount['tax_dict'],
+                    response.cart_amount['grand_total'],
+                );
+
+                // console.log(response.cart_amount['tax_dict'])
+
+                }
+            }
+        })
+    })
+
+    // Place the Quantity on Each Product
+
+    $('.item_qty').each(function(){
+        var the_id = $(this).attr('id')
+        var qty = $(this).attr('data-qty')
+        console.log(qty)
+        $('#'+the_id).html(qty)
+    })
+
+
+    // Decrease Cart
+
+    $('.decrease_cart').on('click', function(e){
+        e.preventDefault();
+        
+        product_id = $(this).attr('data-id');
+        url = $(this).attr('data-url');
+        cart_id = $(this).attr('id');
+
+      
+        $.ajax({
+            type: 'GET',
+            url: url,
+            success: function(response){
+                // alert(response)
+                console.log(response)
+                if(response.status == 'login_required'){
+                    // console.log(response)
+
+                    // Implement SweetAlert Function
+                    swal(response.message, '', 'info').then(function(){
+                        window.location = '/login';
+                    })
+                }
+                else if(response.status == 'Failed'){
+                    swal(response.message, '', 'error')
+
+                }
+                else{
+                     // console.log(response.cart_counter['cart_count'])
+                $('#cart_counter').html(response.cart_counter['cart_count']);
+                $('#qty-'+product_id).html(response.qty);
+
+                applyCartAmounts(
+                    response.cart_amount['subtotal'],
+                    response.cart_amount['tax_dict'],
+                    response.cart_amount['grand_total'],
+                );              
+
+                // This should run only if the user is in the cart page
+
+                if(window.location.pathname == '/cart/'){
+                    removeCartItem(response.qty, cart_id);
+                    checkEmptyCart();
+                }
+
+                
+               
+                }
+            }
+        })
+    })
+
+
+    // Delete Cart
+
+
+    $('.delete_cart').on('click', function(e){
+        e.preventDefault();
+        
+        cart_id = $(this).attr('data-id');
+        url = $(this).attr('data-url');
+
+      
+        $.ajax({
+            type: 'GET',
+            url: url,
+            success: function(response){
+                // alert(response)
+                console.log(response)
+                if(response.status == 'Failed'){
+                    swal(response.message, '', 'error')
+
+                }
+                else{
+                     // console.log(response.cart_counter['cart_count'])
+                $('#cart_counter').html(response.cart_counter['cart_count']);
+                swal(response.status, response.message, "success")
+
+
+                applyCartAmounts(
+                    response.cart_amount['subtotal'],
+                    response.cart_amount['tax_dict'],
+                    response.cart_amount['grand_total'],
+                );
+
+                removeCartItem(0, cart_id)
+                checkEmptyCart();
+                }
+            }
+        })
+    })
+
+    // delete cart element if the quantity is 0
+
+    function removeCartItem(cartItemQty, cart_id){
+        if(cartItemQty <= 0){
+            // remove the cart item element
+            document.getElementById("cart-item-"+cart_id).remove()
+        }
+    }
+
+     // Check if Cart is Empty and Display Cart is Empty 
+     
+     function checkEmptyCart(){
+        var cart_counter = document.getElementById('cart_counter').innerHTML
+        if(cart_counter == 0){
+            document.getElementById("empty-cart").style.display = "block";
+        }
+     }
+
+    //  Function to apply Cart Amounts
+    function applyCartAmounts(subtotal, tax_dict, grand_total){
+        if(window.location.pathname == '/cart/'){
+            $('#subtotal').html(subtotal)
+            $('#total').html(grand_total)
+
+            console.log(tax_dict)
+            // Loop Through THe Tax Dictionary
+            for(key1 in tax_dict){
+                console.log(tax_dict[key1])
+                for(key2 in tax_dict[key1]){
+                    console.log(tax_dict[key1][key2])
+                    $('#tax-'+key1).html(tax_dict[key1][key2])
+                }
+            }
+        }
+    }
+    
+    
+
+    // ADD OPENING HOURS
+    $('.add_hour').on('click', function(e){
+        e.preventDefault();
+        // alert('test');
+        var day = document.getElementById('id_day').value
+        var from_hour = document.getElementById('id_from_hour').value
+        var to_hour = document.getElementById('id_to_hour').value
+        var is_closed = document.getElementById('id_is_closed').checked
+        var csrf_token = $('input[name=csrfmiddlewaretoken]').val()
+        var url = document.getElementById('add_hour_url').value
+
+        console.log(day, from_hour, to_hour, is_closed, csrf_token)
+
+        // Use dynamic condition
+
+        if(is_closed){
+            is_closed = 'True'
+            condition = "day != ''"
+        }else{
+            is_closed = 'False'
+            condition = "day != '' && from_hour != '' && to_hour != ''"
+        }
+
+
+        if(eval(condition)){
+            // s   end ajax request
+            $.ajax({
+                type: 'POST',
+                url: url,
+                data: {
+                    'day': day,
+                    'from_hour': from_hour,
+                    'to_hour': to_hour,
+                    'is_closed': is_closed,
+                    'csrfmiddlewaretoken':csrf_token,
+                },
+                success: function(response){
+                    if(response.status == 'success'){
+                        if(response.is_closed == 'Closed'){
+                            html = '<tr id="hour-'+response.id+'"><td><b>'+response.day+'</b></td><td>Closed</td><td><a href="#" class="remove_hour" data-url="/supplier/opening-hours/remove/'+response.id+'/">Remove</a></td></tr>';
+                        }
+                        else{
+                            // Append the opening hours time to be automatically updated when a vendor enters a day and use Javascript dynamic value we will be getting from the response
+                            html = '<tr id="hour-'+response.id+'"><td><b>'+response.day+'</b></td><td>'+response.from_hour+' - '+response.to_hour+'</td><td><a href="#" class="remove_hour" data-url="/supplier/opening-hours/remove/'+response.id+'/">Remove</a></td></tr>';
+                        }
+                        
+                        $('.opening_hours').append(html)
+                        document.getElementById('opening_hours').reset();
+                    }else{
+                        swal(response.message, '', "error")
+                    }
+                    // console.log(response)
+                }
+            })
+            console.log('add the entry')
+        }else{
+            swal('Please fill all the fields', '', 'info');
+        }
+
+    });
+
+    // REMOVE OPENING HOURS
+    $(document).on('click', '.remove_hour', function(e){
+        e.preventDefault();
+        url = $(this).attr('data-url');
+        
+        $.ajax({
+            type: 'GET',
+            url: url,
+            success: function(response){
+                if(response.status == 'success'){
+                    document.getElementById('hour-'+response.id).remove()
+                }
+            }
+        })
+    })
+
+
+    // document ready close
+        
+})
