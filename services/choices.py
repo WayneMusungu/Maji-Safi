8a20d50d641945a1b1542eac3e656f49448b81ea
wayne <waynemuyera17@gmail.com>
2024-12-18 11:20:34 +0300

    Add choices field option in bottle_size

---
+++ services/choices.py
@@ -0,0 +1,11 @@
+BOTTLE_SIZE = (
+    ('250ML', '250 Milliliters'),
+    ('500ML', '500 Milliliters'),
+    ('300ML', '300 Milliliters'),
+    ('1L', '1 Liter'),
+    ('1.5L', '1.5 Liters'),
+    ('2L', '2 Liters'),
+    ('5L', '5 Liters'),
+    ('10L', '10 Liters'),
+    ('20L', '20 Liters'),
+)
