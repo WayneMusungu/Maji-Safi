8461c1f7cac86495d22a1d9cb38228e91e023b80
wayne <waynemuyera17@gmail.com>
2025-02-06 08:43:18 +0300

    Update on orders utils file

--- orders/utils.py
+++ orders/utils.py
@@ -1,13 +1,14 @@
 import datetime
 import simplejson as json
 
+
 def generate_order_number(pk):
     current_datetime = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
     order_number = current_datetime + str(pk)
     return order_number
 
 
-#Pass the supplier explicitly
+# Pass the supplier explicitly
 def order_total_by_supplier(order, supplier_id):
     total_data = json.loads(order.total_data)
     data = total_data.get(str(supplier_id))
@@ -29,11 +30,7 @@ def order_total_by_supplier(order, supplier_id):
     grand_total = float(subtotal) + float(tax)
     context = {
         'subtotal': subtotal,
-        'tax_dict': tax_dict, 
+        'tax_dict': tax_dict,
         'grand_total': grand_total,
     }
     return context
-
-    
-    
-    
